48be6173b0b30ab9b80e6de72629362e3b62e448
Daniel Silva <daniel.silva@cegal.com>
2022-10-28 14:59:18 +0200

    Added image carousel

---
+++ frontend/src/components/photo_displayers/ImageCarousel.vue
@@ -0,0 +1,38 @@
+<template>
+  <Carousel
+    :settings="settings"
+    :breakpoints="breakpoints"
+    :autoplay="8000"
+    :wrap-around="true"
+  >
+    <Slide v-for="slide in 10" :key="slide">
+      <div class="carousel--image-fallback">{{ slide }}</div>
+    </Slide>
+
+    <template #addons>
+      <Navigation />
+      <Pagination />
+    </template>
+  </Carousel>
+</template>
+
+<script lang="ts" setup>
+//Carousel docs: https://ismail9k.github.io/vue3-carousel/
+import { Carousel, Navigation, Slide, Pagination } from "vue3-carousel";
+import "vue3-carousel/dist/carousel.css";
+
+const settings = {
+  itemsToShow: 1,
+  snapAlign: "center",
+};
+const breakpoints = {
+  800: {
+    itemsToShow: 1.5,
+    snapAlign: "center",
+  },
+  1400: {
+    itemsToShow: 2,
+    snapAlign: "center",
+  },
+};
+</script>
--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,9 +1,51 @@
 <template>
-  <p>About</p>
+  <h1 class="title">About</h1>
+  <section id="about--intro-section">
+    <div class="width--smaller">
+      <div class="img-container--square">
+        <img
+          class="img--square"
+          src="https://picsum.photos/id/0/367/267"
+          alt=""
+        />
+      </div>
+    </div>
+    <div>
+      <p>
+        Yup, that’s me. You’re probably wondering how I got into this
+        situation... Dsakdjn asdkj ns adjna.s, fopnaop3fnfka l skalf a lak f.
+        Qiufaf, asufbpoas afskj nkasnf mf. ams, jan lk amm.dffma.kw fmf.
+      </p>
+      <a class="btn btn--text-primary float-right" href="#about--contact-title">
+        <font-awesome-icon icon="fa-solid fa-arrow-down" />
+        Contact
+      </a>
+    </div>
+  </section>
+  <h2 class="title">Background</h2>
+  <section id="about--background-section">
+    <p>
+      Yup, that’s me. You’re probably wondering how I got into this situation...
+      Dsakdjn asdkj ns adjna.s, fopnaop3fnfka l skalf a lak f. Qiufaf, asufbpoas
+      afskj nkasnf mf. ams, jan lk amm.dffma.kw fmf.
+    </p>
+  </section>
+  <h2 class="title">Process</h2>
+  <section id="about--process-section"></section>
+  <h2 class="title">Gallery</h2>
+  <section id="about--gallery-section">
+    <ImageCarousel />
+  </section>
+  <h2 class="title" id="about--contact-title">Contact</h2>
+  <section id="about--contact-section">
+    <TheContactForm />
+  </section>
 </template>
 
 <script lang="ts" setup>
 import { onBeforeMount } from "vue";
+import ImageCarousel from "@/components/photo_displayers/ImageCarousel.vue";
+import TheContactForm from "@/components/TheContactForm.vue";
 
 onBeforeMount(() =>
   document.documentElement.setAttribute("data-theme", "light")
