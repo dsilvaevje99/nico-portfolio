170c956b7c1f4958c3a8801971342d39db2d423a
Daniel Silva <daniel.silva@cegal.com>
2022-11-04 10:24:50 +0100

    Improved scroll behavior on route changes

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,7 +1,14 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {
+  createRouter,
+  createWebHistory,
+  type RouteLocationNormalized,
+  type RouteRecordName,
+} from "vue-router";
 import FilmsView from "@/views/FilmsView.vue";
 import AboutView from "@/views/AboutView.vue";
 import LoginView from "@/views/LoginView.vue";
+
+const scrollPositions = Object.create(null);
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -34,6 +41,51 @@ const router = createRouter({
       component: LoginView,
     },
   ],
+  scrollBehavior(to, from, savedPosition) {
+    const element = document.body;
+
+    if (
+      savedPosition &&
+      element !== null &&
+      ((to as RouteLocationNormalized).name as RouteRecordName) in
+        scrollPositions
+    ) {
+      // If browser nav-buttons were used
+      element.scrollTop =
+        scrollPositions[
+          (to as RouteLocationNormalized).name as RouteRecordName
+        ];
+    } else {
+      if (to.name === from.name) {
+        // SAME-PAGE: Scroll as normal
+        scrollPositions[
+          (to as RouteLocationNormalized).name as RouteRecordName
+        ];
+      } else if (to.hash) {
+        // ANCHOR ON PAGE LOAD: Scroll as normal
+        const anchor = document.getElementById(to.hash.substring(1));
+        const position = anchor?.getBoundingClientRect();
+        if (anchor && position) element.scrollTop = position.top;
+      } else {
+        // ROUTE CHANGED: Scroll to top right in the middle of animation, so scrolling is invisible
+        setTimeout(() => {
+          document.body.style.scrollBehavior = "initial";
+          element.scrollTop = 0;
+          document.body.style.scrollBehavior = "smooth";
+        }, 249);
+      }
+    }
+  },
+});
+
+router.beforeEach((to, from, next) => {
+  const element = document.body;
+  if (element !== null) {
+    scrollPositions[(from as RouteLocationNormalized).name as RouteRecordName] =
+      element.scrollTop;
+  }
+
+  next();
 });
 
 export default router;
