07d9104a47c178720b15c4e97bd93e2b4e79a670
Daniel Silva <daniel.silva@cegal.com>
2022-11-10 10:24:40 +0100

    Added 404 page

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import FilmsView from "@/views/FilmsView.vue";
 import AboutView from "@/views/AboutView.vue";
 import LoginView from "@/views/LoginView.vue";
+import PageNotFound from "@/views/PageNotFound.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,6 +33,11 @@ const router = createRouter({
       path: "/login",
       name: "login",
       component: LoginView,
+    },
+    {
+      path: "/:catchAll(.*)",
+      name: "404",
+      component: PageNotFound,
     },
   ],
   scrollBehavior(to, from, savedPosition) {
---
+++ frontend/src/views/PageNotFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="not-found--container">
+    <h1 class="title">404</h1>
+    <div>
+      <h2>Page not found</h2>
+      <p>Sorry, this content has either been deleted or removed.</p>
+    </div>
+  </div>
+</template>
