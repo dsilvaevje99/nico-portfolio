96480a40dddd30746ae411adc7c653698f2516e7
Daniel Silva <daniel.silva@cegal.com>
2022-11-17 10:26:26 +0100

    Featured film area

--- frontend/src/components/film_displayers/FilmGridTile.vue
+++ frontend/src/components/film_displayers/FilmGridTile.vue
@@ -1,21 +1,9 @@
 <template>
   <div class="film--tile-thumbnail">
     <img :src="props.film.thumbnail" alt="Thumbnail" />
-    <span v-if="props.film.featured" class="film--featured-badge"
-      >Featured</span
-    >
   </div>
   <h2 class="film--tile-title">{{ props.film.title }}</h2>
   <p class="film--tile-description">{{ props.film.description }}</p>
-  <router-link
-    v-if="props.film.featured"
-    :to="`/film/${props.film.slug}`"
-    class="btn btn--text-primary"
-    tabindex="8"
-  >
-    More about the featured film
-    <font-awesome-icon class="iconRight" icon="fa-solid fa-arrow-right" />
-  </router-link>
 </template>
 
 <script lang="ts" setup>
