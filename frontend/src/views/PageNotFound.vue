a01b17b824e63b6380fa8377adf0cf8da846a891
Daniel Silva <daniel.silva@cegal.com>
2022-12-02 11:25:40 +0100

    Fixed typo

--- frontend/src/views/PageNotFound.vue
+++ frontend/src/views/PageNotFound.vue
@@ -3,7 +3,7 @@
     <h1 class="title">404</h1>
     <div>
       <h2>Page not found</h2>
-      <p>Sorry, this content has either been deleted or removed.</p>
+      <p>Sorry, this content has either been deleted or moved.</p>
     </div>
   </div>
 </template>
