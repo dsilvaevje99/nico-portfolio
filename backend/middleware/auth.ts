a2fe7bb1e96baf55c2798164153687f89600a3d6
Daniel Silva <daniel.silva@cegal.com>
2022-12-20 11:16:45 +0100

    Remove console.log

--- backend/middleware/auth.ts
+++ backend/middleware/auth.ts
@@ -1,16 +1,13 @@
-import { Logger } from "../logger/logger";
 import jwt = require("jsonwebtoken");
 export default (req: any, res: any, next: Function) => {
-  const logger = new Logger();
   try {
     const token = req.headers.authorization.replace("Bearer ", "");
-    logger.info(`Authing with token ${token}`);
     const decoded = jwt.verify(token, "secret");
     req.userData = decoded;
     next();
   } catch (err) {
     return res.status(401).json({
-      message: "Authentification Failed",
+      message: "Authentication Failed",
     });
   }
 };
