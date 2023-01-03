580bb4b85df6ffa8326144f3630f761b4f37669b
Daniel Silva <daniel.silva@cegal.com>
2023-01-03 12:51:18 +0100

    Added links to social icons

--- frontend/src/components/SocialIcons.vue
+++ frontend/src/components/SocialIcons.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="social--container">
-    <font-awesome-icon icon="fa-brands fa-instagram" />
-    <font-awesome-icon icon="fa-brands fa-imdb" />
-    <img alt="Letterboxd" src="@/assets/letterboxd.svg" />
+    <a
+      href="https://www.instagram.com/nicoromerosilva/"
+      target="_blank"
+      aria-label="Instagram"
+      ><font-awesome-icon icon="fa-brands fa-instagram"
+    /></a>
+    <a
+      href="https://www.imdb.com/name/nm13215587/"
+      target="_blank"
+      aria-label="IMDb"
+      ><font-awesome-icon icon="fa-brands fa-imdb"
+    /></a>
+    <a
+      href="https://letterboxd.com/NicoRomero/"
+      target="_blank"
+      aria-label="Letterboxd"
+      ><img alt="Letterboxd Icon" src="@/assets/letterboxd.svg"
+    /></a>
   </div>
 </template>
--- frontend/src/components/TheFooter.vue
+++ frontend/src/components/TheFooter.vue
@@ -1,10 +1,13 @@
 <template>
   <footer>
     <SocialIcons />
-    <p>© 2022 Nicolás Romero</p>
+    <p>© {{ year }} Nicolás Romero</p>
   </footer>
 </template>
 
 <script lang="ts" setup>
+import { computed } from "vue";
 import SocialIcons from "@/components/SocialIcons.vue";
+
+const year = computed(() => new Date().getFullYear());
 </script>
